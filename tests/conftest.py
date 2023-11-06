1dcd3c4d7ab6bebd8e166a4c42e4588e9f09130f
Artem <a.baburkin@it.adcombo.com>
2023-11-06 18:19:03 +0300

    addwalletfix commit

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from selene import browser, be
 from config import ui_base_url, LOGIN, PASSWORD
-from tests.pages import loginpage
+from tests.pages import loginpage, wallet
 
 @pytest.fixture
 def login_pp():
@@ -10,3 +10,11 @@ def login_pp():
     pp_login_page.element(loginpage.input_password).type(PASSWORD)
     pp_login_page.element(loginpage.button_submit).click()
     pp_login_page.wait_until(pp_login_page.element(loginpage.logo).should(be.visible))
+
+@pytest.fixture
+def del_wallet():
+    yield
+    wallet_page = browser.open(f'{ui_base_url}/{wallet.page_url}')
+    wallet_page.element(wallet.button_delete_wallet).click()
+    wallet_page.element(wallet.button_confirm_delete_wallet).click()
+    wallet_page.element(wallet.toast_success2).should(be.visible)
--- tests/ui tests/test_add_wallet.py
+++ tests/ui tests/test_add_wallet.py
@@ -1,23 +1,25 @@
-from tests.conftest import login_pp
+from tests.conftest import login_pp, del_wallet
 from tests.pages import wallet, dashboard
 from selene import browser, be
 from config import ui_base_url, tron_wallet
 
 
-def test_add_wallet(login_pp):
+def test_add_wallet(login_pp, del_wallet):
     wallet_page = browser.open(f'{ui_base_url}/{wallet.page_url}')
     wallet_page.element(wallet.button_add).click()
     wallet_page.element(wallet.select_blockchain).click()
     wallet_page.element(wallet.blockchain_option).click()
     wallet_page.element(wallet.input_address).send_keys(tron_wallet)
     wallet_page.element(wallet.button_submit).click()
-    wallet_page.wait_until(browser.element(wallet.toast_success).should(be.visible))
-    browser.element(wallet.toast_success).should(be.visible)
+    # wallet_page.element(wallet.toast_success).should(be.visible)
     # Проверить что он появился в списке
 
     # Проверка того что кошелёк появился в дашборде
     dashboard_page = browser.open(f'{ui_base_url}/{dashboard.page_url}')
     dashboard_page.element(dashboard.button_add_trc).click()
     dashboard_page.element(dashboard.select_wallet).click()
-    dashboard_page.element(f'//span[contains(text(), {tron_wallet})]').click()
+    dashboard_page.element(f'//span[contains(text(), \'{tron_wallet}\')]').click()
+    dashboard_page.element(dashboard.input_hash).should(be.visible)
+
+    # Проверка что получилось ыбрать этот платёжный метод
     dashboard_page.element(dashboard.input_hash).should(be.visible)
