6f658c530c606e0c3e75d01a1faaee09a56bab74
Artem <a.baburkin@it.adcombo.com>
2023-11-12 12:22:30 +0300

    req

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,14 +4,49 @@ import requests
 from selene import browser, be, query
 from tests.pages import loginpage, wallet, settings
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
+from tests.utils import attach
 
 
 @pytest.fixture(scope='function', autouse=True)
 def config_browser():
     driver = webdriver.Chrome()
     browser.config.base_url = 'https://my.combo.cards/'
-    browser.driver.set_window_size(width=4000, height=2000)
+
+    def setup_browser():
+        # Список всех доступных парамеров https://peter.sh/experiments/chromium-command-line-switches/
+
+        browser.config.window_height = 1400
+        browser.config.window_width = 1600
+        browser_version = "100.0"
+        options = Options()
+        selenoid_capabilities = {
+            "browserName": "chrome",
+            "browserVersion": browser_version,
+            "selenoid:options": {
+                "enableVNC": True,
+                "enableVideo": True
+            }
+        }
+        login = os.getenv('SELEN_LOGIN')
+        password = os.getenv('SELEN_PASSWORD')
+        options.capabilities.update(selenoid_capabilities)
+        driver = webdriver.Remote(
+            command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+            options=options
+        )
+
+        browser.config.driver = driver
+
+        yield
+
+        attach.add_html(browser)
+        attach.add_screenshot(browser)
+        # attach.add_video(browser)
+        attach.add_logs(browser)
+
+        browser.quit()
 
 
 @pytest.fixture()
